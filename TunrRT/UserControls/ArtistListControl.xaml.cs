c091d4b4da08375b5cda51ae8f716ba9abac64db
Hayden <hayden@outlook.com>
2014-11-23 13:12:08 -0500

    Oh my god finally a jump list.

--- TunrRT/UserControls/ArtistListControl.xaml.cs
+++ TunrRT/UserControls/ArtistListControl.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using TunrRT.DataModel;
+using TunrRT.DataModel.Models;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,6 +25,20 @@ namespace TunrRT.UserControls
 		public ArtistListControl()
 		{
 			this.InitializeComponent();
+		}
+	}
+
+	public class ArtistListConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			List<Song> songs = value as List<Song>;
+			return songs.GroupBy(a => a.Artist.ToUpper()[0]);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
