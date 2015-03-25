2d4f126ede9bdce83fb3e37931f5aa91a5c828d0
Hayden McAfee <hayden@outlook.com>
2015-03-25 01:15:19 -0400

    Started work on artist list

---
+++ TunrRT/Models/LibraryLists/AlbumList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TunrLibrary;
+using TunrLibrary.Models;
+
+namespace TunrRT.Models
+{
+    public class AlbumList : LibraryList
+    {
+        /// <summary>
+        /// A SORTED list of unique artists (through song objects) matching filters.
+        /// </summary>
+        public ObservableCollection<Song> Albums { get; set; }
+
+        public AlbumList(DataSource dataSource, string listName, PropertyInfo targetProperty, List<SongFilter> filters)
+            : base(dataSource, listName, targetProperty, filters) {
+            Albums = new ObservableCollection<Song>();
+        }
+
+        /// <summary>
+        /// Updates the album list to the current data.
+        /// </summary>
+        protected override async void Update()
+        {
+            var songs = await GetResults();
+            var albums = songs.GroupBy(s => s.TagAlbum).Select(a => a.First());
+
+            foreach (var album in albums)
+            {
+                Comparison<Song> albumComparison = (x, y) => x.TagAlbum.CompareTo(y.TagAlbum);
+                var albumComparer = Comparer<Song>.Create(albumComparison);
+                var index = Array.BinarySearch<Song>(Albums.ToArray(), album, albumComparer);
+                if (index < 0)
+                {
+                    Albums.Insert(~index, album);
+                }
+            }
+        }
+    }
+}
---
+++ TunrRT/SampleData/SampleAlbumList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TunrLibrary;
+using TunrLibrary.Models;
+using TunrRT.Models;
+
+namespace TunrRT.SampleData
+{
+    public class SampleAlbumList : AlbumList
+    {
+        public SampleAlbumList() : base(null, "Artist", null, null)
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                Albums.Add(new Song()
+                {
+                    TagAlbum = "Some Album " + i
+                });
+            }
+        }
+
+        public SampleAlbumList(DataSource dataSource, string listName, PropertyInfo targetProperty, List<SongFilter> filters)
+            : base(dataSource, listName, targetProperty, filters)
+        {
+        }
+        protected override void Update()
+        {
+            return;
+        }
+    }
+}
