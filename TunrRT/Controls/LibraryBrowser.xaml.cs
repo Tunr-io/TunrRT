d33a5d207eaf04fa17bb8fc461e2b32e6f9b116a
Hayden McAfee <hayden@outlook.com>
2015-03-27 01:03:10 -0400

    Some transition shit

--- TunrRT/Controls/LibraryBrowser.xaml.cs
+++ TunrRT/Controls/LibraryBrowser.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
@@ -29,6 +30,31 @@ namespace TunrRT.Controls
         {
             var clickedList = (e.ClickedItem as LibraryList);
             (DataContext as DataSource).GoBackTo(clickedList);
+        }
+
+        private void ContentControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            var storyboard = new Storyboard();
+            var projectionAnim = new DoubleAnimation()
+            {
+                From = -256,
+                Duration = new TimeSpan(0, 0, 0, 0, 400),
+                EasingFunction = new QuinticEase() { EasingMode = EasingMode.EaseOut }
+            };
+            Storyboard.SetTargetProperty(projectionAnim, "(UIElement.Projection).(PlaneProjection.GlobalOffsetZ)");
+            storyboard.Children.Add(projectionAnim);
+
+            var opacityAnim = new DoubleAnimation()
+            {
+                From = 0,
+                Duration = new TimeSpan(0, 0, 0, 0, 400),
+                EasingFunction = new QuinticEase() { EasingMode = EasingMode.EaseOut }
+            };
+            Storyboard.SetTargetProperty(opacityAnim, "(UIElement.Opacity)");
+            storyboard.Children.Add(opacityAnim);
+
+            Storyboard.SetTarget(storyboard, (DependencyObject)sender);
+            storyboard.Begin();
         }
     }
 }
